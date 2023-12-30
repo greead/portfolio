b76b0bf01d973c955f39397bb796712cbb1c1913
greead <107977445+greead@users.noreply.github.com>
2023-12-29 20:30:45 -0500

    updated experience

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,53 +1,69 @@
 export let experience_items = [
     {
         date:"August 2023 - Present",
-        title:"Graduate Teaching Assistant - Purdue Fort Wayne, Polytechnic Department",
-        body:"As a GTA, I am responsible for teaching an undergraduate programming fundamentals course to students in the Polytechnic department. Unlike many other GTAs, I was trusted to teach this course entirely on my own. I have been constantly trying to find ways to improve the course for future students, whether I teach it or another lecturer takes over.",
+        title:"CS Master's Program - Purdue Fort Wayne, Department of Computer Science",
+        body:"I am currently in my first year of graduate school at Purdue Fort Wayne. It is definitely more difficult than undergrad, but it is also very fun! After completing my Bachelor's degree, I found that I loved research, programming, science, and math. So, I applied to a Master's program that could give me all of that and more! I don't have any regrets so far.",
+        image: "pfw.png",
+        keywords:["Public Speaking", "Data Analytics", "Problem Solving", "Data Visualization", "Machine Learning", "Software Engineering", "GitHub", "Python", "Django", "Svelte", "Software Planning", "Design Patterns", "Architecture Patterns", "RESTful APIs", "Organization"],
+    },
+    {
+        date:"August 2023 - Present",
+        title:"Graduate Teaching Assistant - Purdue Fort Wayne, School of Polytechnic",
+        body:"As a Graduate Teaching Assistant, I teach a fundamentals of programming course in my university's school of Polytechnic. It's a fantastic experience that mixes my interests in teaching, programming, and learning. I've done it for a semester now, but I am already very excited to innovate and improve the course for the next semester!",
         image: "pfw.png",
         keywords:["Python", "Leadership", "Teaching", "Organization", "Patience", "Public Speaking",],
     },
     {
         date:"August 2023 - Present",
-        title:"Teaching Assistant - Purdue Fort Wayne, CS Department",
-        body:"As a TA, I am working closely with a professor to help develop materials for a graduate level data analytics and visualization course. This includes doing several case studies and giving a tutorial alongside them.",
+        title:"Teaching Assistant - Purdue Fort Wayne, Department of Computer Science",
+        body:"For this teaching assistantship, I am working closely with a professor to help them develop materials for a Data Analytics and Visualization course that they are working on. My professor asked me to help him in this endeavor because he enjoyed the effort I put into my work combined with my general aptitude for data analysis. The truth is that I am excited to learn new things and break it down for others to learn it too!",
         image: "pfw.png",
         keywords:["Python", "Pandas", "matplotlib", "Organization", "Data Preprocessing", "Data Analysis", "Data Visualization", "Teaching"],
     },
     {
         date:"August 2023 - December 2023",
         title:"TAP Project - SEAVAC(USA), L.L.C. ",
-        body:"I worked on a TAP project alongside SEAVAC wherein I cleaned, explored, and did a basic regression analysis on SEAVAC's sales data. This project is being used as an example to launch other TAP projects to give PFW students more opportunities to work with real-life data.",
+        body:"I worked on a TAP project alongside SEAVAC wherein I cleaned, explored, and did a basic regression analysis on SEAVAC's sales data. This project is being used as an example to launch other TAP projects to give PFW students more opportunities to work with real-life data. Working with SEAVAC was an incredible experience that tested me in many ways which forced me to adapt and grow.",
         image: "seavac.jpg",
         keywords:["Data Preprocessing", "Data Analysis", "Data Visualization", "Python", "Pandas", "matplotlib", "Sci-Kit Learn", "Machine Learning", "Technical Report Writing", "Documentation"],
     },
     {
         date:"July 2023",
         title:"Camp Assistant - PFW Summer Computing Camp",
-        body:"I was an assistant for the first PFW Summer Computing Camp. At the camp I was responsible for leading the other camp assistants due to my knowledge over all of the computing subjects.",
+        body:"During the summer of 2023, I worked with high school students at the first PFW Summer Computing Camp. The focus of this camp was to not only get students excited in the field of computing, but to also expose them to other computing fields that they might not have known were options. My background made me especially equipped for this because my studies put me in contact with computer engineering, IT, and CS (the computing fields represented at the camp).",
         image: "pfw.png",
         keywords:["Teamwork", "Leadership", "Organization", "Teaching", "Patience"],
     },
     {
+        date:"August 2018 - May 2023",
+        title:"CPET Bachelor's Program - Purdue Fort Wayne, School of Polytechnic",
+        body:"I graduated PFW's Computer Engineering Technology program after 5 years of hard work. In addition to my major in Computer Engineering Technology, I pursued a minor in Math and a minor in Computer Science. I pursued these minors because I found a genuine fondness for Math and Computer Science that was introduced in the program, but not expanded upon. Alongside my studies, I did several extracurricular research projects, participated in a Co-Op, and made many good friends.",
+        image: "pfw.png",
+        keywords:["Public Speaking", "Teamwork", "Organization", "Perseverance", "Electrical Circuit Analysis", "Digital Circuit Analysis", "Embedded Programming", "C", "Microprocessors", "Leadership", "Research", "Technical Writing", "Documentation", "Computer Networking", "Databases", "SQL", "Android Programming", "Information Security", "Java", "HCI", "Figma", "Data Structures", "Calculus", "Discrete Math", "Linear Algebra", "Statistics", "Punctuality", "Image Processing", ],
+    },
+    {
         date:"August 2021 - May 2023",
         title:"Student Grader - Purdue Fort Wayne, Math Department",
-        body:"I graded papers for a statistics class. I was asked to grade for this class because I excelled in the class material. This required more dilligence than might be expected because I had to analyze submissions thoroughly to give partial points where deserved. This also meant that I had to consistently maintain my knowledge of basic statistics.",
+        body:"After passing my statistics course with flying colors, the professor asked me to help him grade for his classes. I was honored and excited to work somewhere in academia, even if it was just grading. Unexpectedly, this constant exposure to statistics altered my approach to science, leading me to insist on using statistics in my research to improve its quality.",
         image:"pfw.png",
         keywords:["Organization", "Problem Solving", "Basic statistics", "Dilligence", "Punctuality"],
     },
     {
         date:"July 2020 - January 2022",
-        title:"Engineering Intern - Nidec Drive Systems",
-        body:"I worked as an intern for the Summer and Fall semesters at Nidec Drive Systems. While here I took on many different projects that spanned many subjects in the mechanical, electrical, computer, and software engineering fields. During this internship, I worked with other interns to enhance the company from testing to development and research to analysis.",
+        title:"Engineering Co-Op - Nidec Drive Systems",
+        body:"I had a co-op with Nidec Drive Systems during the Summer and Fall semesters for two years. While at Nidec, I took on many different projects that spanned many subjects in the worlds of mechanical, electrical, computer, and software engineering. I also learned to work across specialties in a group with other co-ops. This co-op was a great experience and taught me quite a lot.",
         image:"nidec.jpg",
         keywords:["Teamwork", "Research", "Testing", "Big data", "Data Analysis", "Data Visualization", "C", "Microprocessors", "Circuit Design", "VBA + Excel", "Python", "Pandas", "matplotlib", "Dynomometers", "Engineering", "Problem Solving"],
     },
     {
         date:"August 2019 - July 2020",
         title:"Shift Manager - Pizza Hut of Fort Wayne",
-        body:"I worked as a closing manager at Pizza Hut. I was responsible for overseeing and coordinating the back-of-house and the front-of-house; handling all closing operations such as cleaning up, closing the registers, and securing the building for the night; and I was often the only manager at night, so I was also responsible for resolving any issues or discrepancies during my shifts.",
+        body:"I worked as a closing manager at Pizza Hut. I was responsible for overseeing and coordinating the back-of-house and the front-of-house; handling all closing operations such as cleaning up, closing the registers, and securing the building for the night. I enjoyed fostering a good work environment where things got done, but we had fun doing it.",
         image:"pizza_hut.jpg",
         keywords:["Leadership", "Responsibility", "Teamwork", "Hospitality", "Conflict Resolution",],
     },
+]
 
-
+export let portfolio_items = [
+    
 ]
