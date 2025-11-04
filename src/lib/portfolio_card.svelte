0426e41071422a02278df625ee06b82c887d0b2e
greead04 <greead04@pfw.edu>
2025-11-03 19:06:50 -0500

    fixed spacing and alignment issues with portfolio captions and cards

--- src/lib/portfolio_card.svelte
+++ src/lib/portfolio_card.svelte
@@ -7,7 +7,7 @@
         <img class="portfolio-img" src="/{image}" alt="Project" />
         <figcaption>
             <img class="tooling-icon" src="/tooling_icon.svg" alt="icon" />
-            {tools.join(", ")}
+            <span class="tooling-list">{tools.join(", ")}</span>
         </figcaption>
     </figure>
     <h4 class="title"><b>{title}</b></h4>
@@ -21,10 +21,9 @@
         flex-flow: column nowrap;
         align-items: center;
         background-color: var(--color_bg);
-        opacity: 0.95;
         border: 1px solid green;
         box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-        width: 46%;
+        width: 45%;
         padding: 1rem;
         margin-bottom: 1rem;
         text-align: justify;
@@ -52,18 +51,19 @@
     }
 
     figcaption {
+        display: flex;
+        flex-flow: row nowrap;
         background-color: #333;
-        opacity: 95%;
         color: mintcream;
-        padding: 3px 5px;
         text-align: left;
         align-self: flex-start;
-        max-width: initial;
+        width: 100%;
+        align-items: center;
     }
 
     .tooling-icon {
         height: 1em;
-        padding-right: 5px;
+        padding: 5px 5px;
     }
 
     .title {
