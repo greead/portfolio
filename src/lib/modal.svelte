3f650e83280630183a64563872868433086428c7
greead04 <greead04@pfw.edu>
2025-11-03 20:31:24 -0500

    cleaned up modal, cleaned up filter button styling

--- src/lib/modal.svelte
+++ src/lib/modal.svelte
@@ -1,43 +1,62 @@
 <script>
-    let { text = "", img = "/whispers.png"} = $props();
+    let { text = "TEST", img = "/bytebuddy.png" } = $props();
 </script>
 
 <div id="modal" class="modal">
     <span class="close">×</span>
-    <img class="modal-content" id="img01" alt="modal" src={img}/>
-    <p>{text}</p>
+    <div class="modal-content">
+        <img id="img01" alt="modal" src={img} />
+        <p>{text}</p>
+    </div>
 </div>
 
 <style>
     .modal {
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
         position: fixed;
         top: 10vh;
         left: 10vw;
         z-index: 2;
-        width: 80vw;
-        height: 80vh;
-        opacity: 0.95;
-        background-color: #333;
+        width: 80%;
+        min-height: 80%;
+        background-color: rgba(33, 33, 33, 0.95);
+        
+        border-radius: 15px;
     }
 
     .modal-content {
-        margin: 15% auto;
-        display: block;
-        width: 80%;
-        max-width: 700px;
-        opacity: 100;
+        display: flex;
+        flex-flow: column wrap;
+        justify-content: space-evenly;
+        align-content: center;
+        width: 100%;
+
+        img {
+            max-height: 90%;
+            max-width: 90%;
+            border-radius: 15px;
+        }
+
+        p {
+            text-align: center;
+            color: mintcream
+        }
+        
     }
 
     .close {
-        color: #aaa;
-        float: right;
+        color: mintcream;
+        align-self: flex-end;
         font-size: 28px;
         font-weight: bold;
+        padding: 5px 10px;
     }
 
     .close:hover,
     .close:focus {
-        color: black;
+        color: var(--color_font_secondary);
         text-decoration: none;
         cursor: pointer;
     }
