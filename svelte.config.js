14568bebeb70a1f011c8c0af73c3e48198144b53
greead04 <greead04@pfw.edu>
2025-11-10 20:50:58 -0500

    fixes for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/greead' : '',
+            base: process.argv.includes("dev") ? '' : '',
         }
 	}
 };
